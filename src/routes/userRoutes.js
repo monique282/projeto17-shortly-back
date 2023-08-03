a6e77516051fd8956961fe948dbf12173b4f3c01
monique <moniquejp16@gmail.com>
2023-08-03 16:34:48 -0300

    feat: criando a rota de login, userRoutes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { registerTable } from "../schemas/userSchema.js";
+import { loginTable, registerTable } from "../schemas/userSchema.js";
 import { validateJoiForAll } from "../middlewares/validateSchema.js";
 import { loginPost, registerPost } from "../controllers/controlRegistrationLogin.js";
 
 const userRouter = Router();
 
 userRouter.post("/signup", validateJoiForAll(registerTable), registerPost);
-userRouter.post("/signin", validateJoiForAll(registerTable), loginPost);
+userRouter.post("/signin", validateJoiForAll(loginTable), loginPost);
 
 export default userRouter;
