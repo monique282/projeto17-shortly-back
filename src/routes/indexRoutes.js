8f3cdea99e770ba3fb03e22313749171235f7c8e
monique <moniquejp16@gmail.com>
2023-08-03 14:02:39 -0300

    feat: criando o indexRoutes

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -2,21 +2,13 @@
 // lebrando que todas as Rotas aqui vai pro app
 
 import { Router } from "express";
-import gameRouter from "./gameRoutes.js";
-import customerRouter from "./customersRoutes.js";
-import rentsRouter from "./rentsRoutes.js";
+import userRouter from "./userRoutes.js";
 
 const router = Router()
 
 router.use([
     // rota para os cadastro
-    gameRouter,
-
-    // rota para os clientes
-    customerRouter,
-    
-    // rota para os alugueis
-    rentsRouter
+    userRouter
 ]);
 
 export default router;
