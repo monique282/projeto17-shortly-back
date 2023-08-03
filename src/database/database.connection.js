9fc4eca8ad09c9e7e146b558f7d6d31dfcd7bdc5
monique <moniquejp16@gmail.com>
2023-08-03 07:28:15 -0300

    feat: configurando database

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -1,7 +1,6 @@
 /* conectando o pg */
 import pg from "pg";
 import dotenv from "dotenv";
-
 dotenv.config();
 
 const { Pool } = pg;
@@ -9,7 +8,5 @@ const { Pool } = pg;
 const configDatabase = {
   connectionString: process.env.DATABASE_URL,
 };
-
-if (process.env.NODE_ENV === "production") configDatabase.ssl = true;
 
 export const db = new Pool(configDatabase);
