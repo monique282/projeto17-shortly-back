19d894e04b19bea8bdcce124f2dc05eb73d65a8e
monique <moniquejp16@gmail.com>
2023-08-04 14:08:01 -0300

    feat: criando função de quarda a url e o shorUrl, controlUrls

--- src/controllers/controlUrls.js
+++ src/controllers/controlUrls.js
@@ -7,14 +7,35 @@ export async function urlsPost(req, res) {
     const { authorization } = req.headers;
     const token = authorization?.replace("Bearer ", "");
 
-    // pegando os dados enviado pelo usuario
-    const {url} = req.bady;
+    // pegando os dados enviado pelo usuario pelo input
+    const { url } = req.bady;
+
 
     try {
+
+        // validando o token
         const userLogeed = await db.query('SELECT * FROM usersLogged WHERE token = $1;', [token]);
-        if(userLogeed.rows.length === 0){
+        if (userLogeed.rows.length === 0) {
             return res.status(401).send({ message: "Usuário não autorizado." });
-        }
+        };
+
+        // gera a short URL utilizando o nanoid
+        const shortUrl = nanoid(8);
+
+        // contando quantas vezes foi visitdo
+        const visitCount = 0
+
+        // enviando os dados para o servidor
+        await db.query('INSERT INTO urls (shortUrl,url,visitCount,Iduser) VALUES ($1, $2, $3)', [shortUrl, url, userLogeed.rows[0].id, visitCount ]);
+
+        // pegando o id
+        const idUrls = await db.query('SELECT * FROM urls WHERE shortUrl = $1;', [shortUrl]);
+        return res.status(201).send({ "id": idUrls.rows[0].id, "shortUrl": shortUrl });
+
+        // shortUrl TEXT NOT NULL,
+        // url TEXT NOT NULL,
+        // visitCount INTEGER NOT NULL,
+        // Iduser INTEGER 
 
     } catch (erro) {
         res.status(500).send(erro.message);
