53221b992330b4fb83f7a7c117e70b0635833701
monique <moniquejp16@gmail.com>
2023-08-04 20:19:16 -0300

    feat: criando a rota de deletar, urlsRoutes

--- src/routes/urlRoutes.js
+++ src/routes/urlRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { urlSentByUser } from "../schemas/urlSchema.js";
-import { urlsGet, urlsOpenGet, urlsPost } from "../controllers/controlUrls.js";
+import { urlsDelete, urlsGet, urlsOpenGet, urlsPost } from "../controllers/controlUrls.js";
 import { validateJoiForAll } from "../middlewares/validateSchema.js";
 
 
@@ -9,7 +9,7 @@ const urlRouter = Router();
 urlRouter.post("/urls/shorten", validateJoiForAll(urlSentByUser), urlsPost);
 urlRouter.get("/urls/:id", urlsGet);
 urlRouter.get("/urls/open/:shortUrl", urlsOpenGet);
-
+urlRouter.delete("/urls/:id", urlsDelete);
 
 
 export default urlRouter;
