f98e9c2288e957a071dc36773f200836eb89ddf1
monique <moniquejp16@gmail.com>
2023-08-05 14:56:50 -0300

    fix: fazendo a importação da função userMeGet, userRoutes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { loginTable, registerTable } from "../schemas/userSchema.js";
 import { validateJoiForAll } from "../middlewares/validateSchema.js";
-import { loginPost, registerPost } from "../controllers/controlUsers.js";
+import { loginPost, registerPost, userMeGet } from "../controllers/controlUsers.js";
 
 const userRouter = Router();
 
 userRouter.post("/signup", validateJoiForAll(registerTable), registerPost);
-userRouter.post("/signin", validateJoiForAll(loginTable), loginPost);
+userRouter.post("/signin", validateJoiForAll(loginTable), userMeGet);
 userRouter.get("/users/me", loginPost);
 
 export default userRouter;
